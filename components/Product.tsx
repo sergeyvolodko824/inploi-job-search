63e5f7391a8402abb2831fbc9dfd3529c44e7051
Sergey Volodko <volodkosergey0@gmail.com>
2021-08-24 15:06:42 +0200

    added the typescript interfaces

--- components/Product.tsx
+++ components/Product.tsx
@@ -57,21 +57,46 @@ const ApplyButton = styled.span`
   }
 `;
 
-const Product = ({ hit }) => {
+//Interfaces
+interface Data {
+  hit: {
+    data_company: {
+      company_name: string;
+      company_logo: string;
+      company_industry: string;
+      company_type: string;
+    };
+    data_job: {
+      job_description: string;
+      job_type: string;
+      job_title: string;
+      job_wage_currency: string;
+      job_wage: string;
+      job_wage_type: string;
+      job_views: string;
+    };
+    data_location: {
+      location_address_1: string;
+      location_country: string;
+    }
+  }
+}
+
+const Product = (props : Data ) => {
   // console.log(hit);
   return (
     <ResultContainer>
       <Divs>
-      <Logo src={hit.data_company.company_logo} />
-      <CompanyName>{hit.data_company.company_name}</CompanyName>
+      <Logo src={props.hit.data_company.company_logo} />
+      <CompanyName>{props.hit.data_company.company_name}</CompanyName>
       </Divs>
       <Divs>
-        <Paragraph>{hit.data_company.company_industry}</Paragraph>
-        <Paragraph>{hit.data_company.company_type}</Paragraph>
+        <Paragraph>{props.hit.data_company.company_industry}</Paragraph>
+        <Paragraph>{props.hit.data_company.company_type}</Paragraph>
       </Divs>
       <Divs>
-        <Paragraph>{hit.data_job.job_description}</Paragraph>
-        <Paragraph><ColoredText>Type:</ColoredText>{hit.data_job.job_type} <ColoredText>Title:</ColoredText>{hit.data_job.job_title} <ColoredText>Wage:</ColoredText>{ hit.data_job.job_wage_currency}{ hit.data_job.job_wage}/{hit.data_job.job_wage_type} <ColoredText>Views:</ColoredText>{hit.data_job.job_views} <ColoredText>Location:</ColoredText>{hit.data_location.location_address_1},{hit.data_location.location_country} </Paragraph>
+        <Paragraph>{props.hit.data_job.job_description}</Paragraph>
+        <Paragraph><ColoredText>Type:</ColoredText>{props.hit.data_job.job_type} <ColoredText>Title:</ColoredText>{props.hit.data_job.job_title} <ColoredText>Wage:</ColoredText>{ props.hit.data_job.job_wage_currency}{ props.hit.data_job.job_wage}/{props.hit.data_job.job_wage_type} <ColoredText>Views:</ColoredText>{props.hit.data_job.job_views} <ColoredText>Location:</ColoredText>{props.hit.data_location.location_address_1},{props.hit.data_location.location_country} </Paragraph>
       </Divs>
       <Divs><ApplyButton>Apply</ApplyButton></Divs>
     </ResultContainer>
